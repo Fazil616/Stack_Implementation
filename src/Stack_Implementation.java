8be586f8a9cde8b123de07b6137417ad15da698f
Fazil616 <fazil616shaikh@gmail.com>
2022-09-24 13:45:59 +0530

    Initial commit

---
+++ src/Stack_Implementation.java
@@ -0,0 +1,73 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack_Implementation {
+
+    public static void main(String[] args){
+        Stack<String> stackElement = new Stack<String>();
+        stackElement.push("BMW");
+        stackElement.push("VOLVO");
+        stackElement.push("AUDI");
+        stackElement.push("FORD");
+        stackElement.push("TESLA");
+        System.out.println(stackElement);
+
+        System.out.println("Please select which function you want to perform:");
+
+
+        //adding elements is stack using function
+        addStackElement(stackElement);
+
+        //poping Element from stack using function
+        popStackElement(stackElement);
+
+    }
+
+    public static void addStackElement(Stack<String> a){
+        Scanner input = new Scanner(System.in);  // Create a Scanner class object
+        System.out.println("Enter List Elements");
+
+        String s;
+
+
+        String elementsToInput = input.nextLine();  // Read the user input
+        System.out.println("Enter the limit of elements you want to push in stack: " + elementsToInput);
+
+        int b= Integer.parseInt(elementsToInput);
+
+
+        for(int i=1;i<=b;i++){
+
+            s= input.nextLine();
+            a.push(s);
+            System.out.println(a);
+        }
+        System.out.println("Stack is:"+a);
+    }
+
+    public static void popStackElement(Stack<String> a){
+
+        Scanner input = new Scanner(System.in);  // Create a Scanner class object
+        System.out.println("Enter Elements you want to pop");
+
+
+
+
+
+
+        String elementsToInput = input.nextLine();  // Read the user input
+
+//        System.out.println("Enter the limit of elements you want to push in stack: " + elementsToInput);
+
+        int b= Integer.parseInt(elementsToInput);
+
+
+        for(int i=1;i<=b;i++){
+
+            System.out.println(a.pop());
+
+        }
+        System.out.println("Stack after poped Elements are:"+a);
+    }
+}
